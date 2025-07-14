eb18b28ae56e1ea9d3d11b5f7864a478c6b43ae0
moroninavarro <moronialanis23@gmail.com>
2025-07-13 21:28:39 -0700

    change

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./productDetails.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 
 const dataSource = new ProductData("tents");
@@ -8,7 +8,7 @@ const productID = getParam("product");
 
 const product = new ProductDetails(productID, dataSource);
 product.init();
-// // add to cart button event handler
+// add to cart button event handler
 // async function addToCartHandler(e) {
 //   const product = await dataSource.findProductById(e.target.dataset.id);
 //   addProductToCart(product);
--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -30,19 +30,22 @@ export default class ProductDetails {
 
 }
     
-    
-    function productDetailsTemplate(product) {
+
+function productDetailsTemplate(product) {
+       
         document.querySelector("h2").textContent = product.Brand.Name;
+       
         document.querySelector("h3").textContent = product.NameWithoutBrand;
 
         const productImage = document.getElementById("productImage");
         productImage.src = product.Image;
         productImage.alt = product.NameWithoutBrand;
-
         document.getElementById("productPrice").textContent = product.FinalPrice;
+       
         document.getElementById("productColor").textContent = product.Colors[0].ColorName;
+        
+
         document.getElementById("productDesc").innerHTML = product.DescriptionHtmlSimple;
-
-
         document.getElementById("addToCart").dataset.id = product.Id;
-    }
+       
+}
