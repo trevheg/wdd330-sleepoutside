3597f3425356adb66c0782472b0f85625bda4237
moroninavarro <moronialanis23@gmail.com>
2025-07-19 02:38:24 -0700

    change

--- src/js/cart.js
+++ src/js/cart.js
@@ -6,6 +6,17 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  
+  if (cartItems.length > 0) {
+    const footer = document.querySelector('.list-footer');
+    footer.style.display = 'block';
+    const totalSpan = document.querySelector('.list-total');
+    const total = cartItems.reduce((sum, item) => sum + item.FinalPrice, 0);
+    totalSpan.textContent = total.toFixed(2);
+  } else{
+    footer.style.display = 'none';
+  }
 }
 
 function cartItemTemplate(item) {
