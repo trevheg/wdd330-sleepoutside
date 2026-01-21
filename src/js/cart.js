9504bab99a60a68872199807ca1147a0813e2b48
Sai <contact.mosiah.1994@gmail.com>
2026-01-21 17:50:39 +0800

    Individual Task - Week 2 - Remove from Cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,12 +1,14 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  const cartItems = getLocalStorage("so-cart") || [];
+  const htmlItems = cartItems.map((item, index) => cartItemTemplate(item, index));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  setupRemoveListeners();
 }
 
-function cartItemTemplate(item) {
+function cartItemTemplate(item, index) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
@@ -20,9 +22,30 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <span class="cart-card__remove" data-index="${index}">X Remove</span>
 </li>`;
 
   return newItem;
+}
+
+function removeFromCart(index) {
+  let cartItems = getLocalStorage("so-cart") || [];
+  cartItems.splice(index, 1);
+  
+  setLocalStorage("so-cart", cartItems);
+  
+  renderCartContents();
+}
+
+function setupRemoveListeners() {
+  const removeButtons = document.querySelectorAll(".cart-card__remove");
+  
+  removeButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+      const index = parseInt(event.target.dataset.index);
+      removeFromCart(index);
+    });
+  });
 }
 
 renderCartContents();
