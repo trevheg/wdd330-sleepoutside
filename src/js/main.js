bc7b9a529f53f551b5d67acf9981fd214208fce7
trevheg <trevorhegstrom@gmail.com>
2026-01-27 21:53:42 -0800

    Got through step 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,4 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
-
 
 loadHeaderFooter();
 
-const element = document.querySelector(".product-list");
-const productData = new ProductData("tents");
-const productList = new ProductList("tents", productData, element);
-
-productList.init();
---
+++ src/js/product-listing.js
@@ -0,0 +1,11 @@
+import { loadHeaderFooter } from "./utils.mjs"
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+loadHeaderFooter();
+
+const element = document.querySelector(".product-list");
+const productData = new ProductData("tents");
+const productList = new ProductList("tents", productData, element);
+
+productList.init();
